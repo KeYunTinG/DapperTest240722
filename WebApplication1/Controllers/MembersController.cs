71a6b454915248a7ef6660fae56279c5d1f8eb64
quinnlee <quinnlee@UT11302111>
2024-07-22 16:14:23 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/MembersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+using Dapper;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly string _connectString = @"Server=(LocalDB)\MSSQLLocalDB;Database=myDB;Trusted_Connection=True;";
+
+        public MembersController(string connectString)
+        {
+            _connectString = connectString;
+        }
+        // GET: api/<MembersController>
+        [HttpGet]
+        public IEnumerable<Member> Get()
+        {
+            using (var conn = new SqlConnection(_connectString))
+                return conn.Query<Member>("SELECT * FROM Members");
+        }
+        // GET api/<MembersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MembersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MembersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MembersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
